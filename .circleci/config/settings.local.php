472ccb6309fd0b86cab34b4e0346c5a8d9cb9886
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-08 21:11:17 +0200

    Circle CI first files.

---
+++ .circleci/config/settings.local.php
@@ -0,0 +1,16 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+if (empty($settings['hash_salt'])) {
+  $settings['hash_salt'] = 'drupal-ci';
+}
