f2125fbc3233e7586a86c6845a2970e08db9e057
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-12 21:07:24 +0200

    Create DB if not exists.

--- RoboFile.php
+++ RoboFile.php
@@ -263,7 +263,7 @@ class RoboFile extends \Robo\Tasks {
   {
     $force = true;
     $tasks = [];
-    $tasks[] = $this->taskExec('mysql -u root -proot -h mariadb -e "create database drupal"');
+    $tasks[] = $this->taskExec('mysql -u root -proot -h mariadb -e "create database if not exists drupal"');
     $tasks[] = $this->taskFilesystemStack()
       ->copy('.gitlab-ci/settings.local.php', 'web/sites/default/settings.local.php', $force);
     $tasks[] = $this->taskExec('wget -O dump.sql "' . getenv('DB_DUMP_URL') . '"');
