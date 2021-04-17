ad133698d97f069a1c87db1c6ad6117196379cc1
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-17 21:43:07 +0200

    Full permissions.

--- .travis/RoboFile.php
+++ .travis/RoboFile.php
@@ -183,8 +183,8 @@ class RoboFile extends \Robo\Tasks
         $tasks[] = $this->taskDockerComposeExec('mkdir -p ' . dirname(static::BASE_PATH));
         $tasks[] = $this->taskDockerComposeExec('chown -R www-data:www-data ' . static::MOUNT_PATH);
         $tasks[] = $this->taskDockerComposeExec('ln -sf ' . static::MOUNT_PATH . '/web ' . static::BASE_PATH);
-        $tasks[] = $this->taskDockerComposeExec('chown -h www-data:www-data ' . static::BASE_PATH);
-        $tasks[] = $this->taskDockerComposeExec('ls -la ' . static::BASE_PATH);
+        $tasks[] = $this->taskDockerComposeExec('chmod -R 777 ' . static::MOUNT_PATH);
+        // $tasks[] = $this->taskDockerComposeExec('chown -h www-data:www-data ' . static::BASE_PATH);
         $tasks[] = $this->taskDockerComposeExec('service apache2 start');
         return $tasks;
     }
